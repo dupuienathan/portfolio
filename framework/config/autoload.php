e5aec60c4e3516af81d2ecbac7a9404db1358448
dupuienathan <dupuienathan@gmail.com>
2016-05-23 01:08:14 -0400

    Add files via upload

---
+++ framework/config/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+// These libraries will be automatically loaded when the framework is created
+$config["autoload"]["libraries"] = array(array("database", "db"));
---
+++ framework/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$config["database"]["default"] = "main";
+
+$config["database"]["main"]["hostname"] = "localhost";
+$config["database"]["main"]["database"] = "if_test";
+$config["database"]["main"]["username"] = "root";
+$config["database"]["main"]["password"] = "";
+$config["database"]["main"]["port_num"] = 3306;
+$config["database"]["main"]["dbdriver"] = "mysql";
